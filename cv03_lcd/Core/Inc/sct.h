2065a4d4906138f3325a6d598f18377176e980d0
alexbekec <alexander.bekec@gmail.com>
2023-10-12 15:43:43 +0200

    cv03: Commit 001

---
+++ cv03_lcd/Core/Inc/sct.h
@@ -0,0 +1,19 @@
+/*
+ * sct.h
+ *
+ * SCT LIBRARY HEADER FILE
+ * ALEXANDER BEKEC - xbekec00 - 221096
+ *
+ *  Created on: Oct 12, 2023
+ *      Author: xbekec00
+ */
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+// FUNCTION PROTOTYPES
+void sct_init(void);
+void sct_led(uint32_t value);
+void sct_value(uint16_t value);
+
+#endif /* INC_SCT_H_ */
